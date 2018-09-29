1d6c1466ecce6fa39162594c669ee2fc1bcf2f15
JoaoPaulo-PC <johnny-gomes84@yahoo.com>
2018-09-29 00:20:24 -0300

    commit inicial

---
+++ src/main/java/br/com/javaparaweb/financeiro/util/RNException.java
@@ -0,0 +1,29 @@
+package br.com.javaparaweb.financeiro.util;
+
+public class RNException extends Exception {
+
+	public RNException() {
+		
+	}
+
+	public RNException(String message) {
+		super(message);
+		
+	}
+
+	public RNException(Throwable cause) {
+		super(cause);
+		
+	}
+
+	public RNException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public RNException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		
+	}
+
+}
